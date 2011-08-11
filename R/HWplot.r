92d3f97145f6d2a58b4799e45e0c6d56c86d69f3
Paul J. McMurdie <mcmurdie@stanford.edu>
2011-08-11 14:56:55 -0700

    some more tests. Woo!

--- R/HWplot.r
+++ R/HWplot.r
@@ -4,15 +4,19 @@
 #' @param ... the same arguments that one would provide
 #' to qplot.
 #' @return A helloworld-ified qplot. Oh, you'll see.
-#' @seealso qplot ggplot2
+#' @seealso \code{\link{plot}} qplot ggplot2
 #' @import ggplot2
 #' @export
+<<<<<<< HEAD
 #' @examples
 #' library("ggplot2")
 #' qplot(carat, price, color=color, data = diamonds, facets=cut ~ color,
 #'   main="Price versus carat, according to color and cut")
 #' HWplot(carat, price, color=color, data = diamonds, facets=cut ~ color,
 #'   main="Price versus carat, according to color and cut")
+=======
+#' @examples #
+>>>>>>> some more tests. Woo!
 HWplot <- function(...){
 	#arguments <- list(...)
 	arguments <- as.list(match.call()[-1])	
--- R/class.r
+++ R/class.r
@@ -1,11 +1,17 @@
 ##############################################################
 #' Create a class for strings that should print HW to screen.
 #' 
+<<<<<<< HEAD
 #' Inherits the "character" class. And really nothing else.
 #'
 #' @export
 #' @examples
 #' getSlots("helloworld")
+=======
+#' Inherits the "character" class.
+#' 
+#' @examples #
+>>>>>>> some more tests. Woo!
 setClass("helloworld", contains = "character")
 ##############################################################
 # #' @slot .Data character-class data.
